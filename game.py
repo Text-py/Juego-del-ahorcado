0455614fb30275b0608b21f30c7023630585d1d2
Sourcery AI <>
2022-07-20 03:35:30 +0000

    'Refactored by Sourcery'

--- game.py
+++ game.py
@@ -55,7 +55,7 @@ def run():
  / \  |
       |
 =========''']
-    
+
     DB = [
         "C",
         "PYTHON",
@@ -68,38 +68,35 @@ def run():
         "XD",
         "HOLA"
     ]
-    
+
     word = random.choice(DB)
     spaces = ["_"] * len(word)
     attemps = 6
-    
+
     while True:
         os.system("cls")
         for character in spaces:
             print(character, end="")
         print(IMAGES[attemps])
         letter = input("Elige una letra").upper()
-        
+
         found = False
         for idx, character in enumerate(word):
             if character == letter:
                 spaces[idx] = letter
                 found = True
-        
+
         if not found:
             attemps -= 1
-           
+
         if "_" not in spaces:
             os.system("cls")
             print("Ganaste")
             break
-            input() 
-            
         if attemps == 0:
             os.system("cls")
             print("Perdiste")
-            break
-            input() 
+            break 
         
         
         
